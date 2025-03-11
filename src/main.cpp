e24b2278db58900efd70558e324ec2d21bc5f1f0
Mario Gottardo <mario.gottardo27@gmail.com>
2025-03-11 17:37:00 +0100

    added sample main

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <netdb.h>
+#include <iostream>
+#include <cstdlib>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "self-cert-bot/Client.h"
+#include "self-cert-bot/Server.h"
+
+#define BUF_SIZE 500
+
+int main(const int argc, char *argv[]) {
+
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+
+    if (argc < 3) {
+        exit(EXIT_FAILURE);
+    }
+
+    const std::string domain = argv[1];
+
+    if (const std::string mode = argv[2]; mode == "client") {
+        const auto client = certbot::Client("test.com");
+        client.start();
+    } else if (mode == "server") {
+        const std::string cert_path = argv[3];
+        const auto server = certbot::Server(domain, cert_path);
+        server.start();
+    }
+
+    exit(EXIT_SUCCESS);
+}
