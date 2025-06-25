834647924a01b135069fad0c2e1949b85570791a
Mario Gottardo <mario.gottardo27@gmail.com>
2025-06-25 22:45:31 +0200

    TLS proof of concept client side

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -10,36 +10,52 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/types.h>
 #include <sys/socket.h>
+
+#include "self-cert-bot/utils.h"
 
 namespace certbot {
     void Client::start() const {
-        const int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
         std::cout << "server domain " << domain << std::endl;
 
-        sockaddr_in serverAddress{};
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_port = htons(8080);
-        serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // inet_addr("127.0.0.1")
+        const SSL_METHOD* method = TLS_client_method();
+        SSL_CTX *ctx = SSL_CTX_new(method);
+        if (!ctx) {
+            std::cerr << "Failed to create SSL context\n";
+        }
 
-        const int status = connect(clientSocket, reinterpret_cast<sockaddr *>(&serverAddress), sizeof(serverAddress));
+        SSL *ssl = SSL_new(ctx);
 
-        std::cout << "client status: " << status << std::endl;
+        const int clientSocket = setup_socket_client();
+        if (SSL_set_fd(ssl, clientSocket) == 0) {
+            std::cerr << "Failed to bound the file descriptor\n";
+            ERR_print_errors_fp(stderr);
+        }
+
+        if (const int ret = SSL_connect(ssl); ret != 1) {
+            std::cerr << "Failed to connect\n";
+            ERR_print_errors_fp(stderr);
+        }
 
         bool flag = true;
-        const auto hello_message = "Hej!";
-        send(clientSocket, hello_message, strlen(hello_message), 0);
+        const std::string hello_message = "Hej!";
+        SSL_write(ssl, hello_message.c_str(), strlen(hello_message.c_str()));
 
         while (flag) {
             std::string message;
             std::cout << "$> ";
             std::cin >> message;
-            send(clientSocket, message.c_str(), strlen(message.c_str()), 0);
+            SSL_write(ssl, message.c_str(), strlen(message.c_str()));
             if (message == "exit") {
                 flag = false;
             }
         }
 
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
         close(clientSocket);
 
         exit(EXIT_SUCCESS);
