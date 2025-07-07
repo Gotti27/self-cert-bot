b2e1cb69ce5a3f564c3e7afde19b00cc7cd73ec6
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2025-07-07 08:01:17 +0200

    Testing support with Boost (#6)

---
+++ tests/utils/test_utils.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Mario Gottardo on 26/06/25.
+//
+
+#define BOOST_TEST_MODULE SelfCertBotTests
+#include <boost/test/included/unit_test.hpp>
+
+#include "self-cert-bot/utils.h"
+
+BOOST_AUTO_TEST_CASE(RandomStringLength) {
+    BOOST_CHECK(generate_random_string(12).size() == 12);
+}
+
