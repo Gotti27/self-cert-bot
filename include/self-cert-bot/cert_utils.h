9de92ddee41a137e61b8eb65d299e0925cdfa217
Mario Gottardo <mario.gottardo27@gmail.com>
2025-07-27 11:47:25 +0200

    Added certbot namespace to cert_utils

--- include/self-cert-bot/cert_utils.h
+++ include/self-cert-bot/cert_utils.h
@@ -8,32 +8,34 @@
 #include <vector>
 #include <openssl/types.h>
 
-typedef struct CertFieldsStruct {
-    std::string C;
-    std::string ST;
-    std::string O;
-    std::string OU;
-    std::string CN;
-} CertFields;
+namespace certbot {
+    typedef struct CertFieldsStruct {
+        std::string C;
+        std::string ST;
+        std::string O;
+        std::string OU;
+        std::string CN;
+    } CertFields;
 
-EVP_PKEY* generate_keypair();
+    EVP_PKEY *generate_keypair();
 
-X509* generate_certificate(EVP_PKEY* pkey, const CertFields& cert_fields);
+    X509 *generate_certificate(EVP_PKEY *pkey, const CertFields &cert_fields);
 
-void sign_child_certificate(const X509* ca_cert, EVP_PKEY* ca_pkey, X509* child_cert, EVP_PKEY* child_pkey);
+    void sign_child_certificate(const X509 *ca_cert, EVP_PKEY *ca_pkey, X509 *child_cert, EVP_PKEY *child_pkey);
 
-void save_certificate(const X509* cert, const char* filename);
+    void save_certificate(const X509 *cert, const char *filename);
 
-void save_key(const EVP_PKEY* pkey, const char* filename);
+    void save_key(const EVP_PKEY *pkey, const char *filename);
 
-int craft_certificate(const X509* ca_cert, EVP_PKEY* ca_pkey, X509*& child_cert, EVP_PKEY*& child_pkey, const CertFields& cert_fields);
+    int craft_certificate(const X509 *ca_cert, EVP_PKEY *ca_pkey, X509 *&child_cert, EVP_PKEY *&child_pkey,
+                          const CertFields &cert_fields);
 
-std::string X509ToPEMString(const X509* cert);
+    std::string X509ToPEMString(const X509 *cert);
 
-std::vector<unsigned char> serializeX509ToDER(const X509* cert);
+    std::vector<unsigned char> serializeX509ToDER(const X509 *cert);
 
-std::vector<unsigned char> serializePrivateKey(const EVP_PKEY* pkey);
+    std::vector<unsigned char> serializePrivateKey(const EVP_PKEY *pkey);
 
-EVP_PKEY* deserializePrivateKey(const std::vector<unsigned char>& buffer);
-
+    EVP_PKEY *deserializePrivateKey(const std::vector<unsigned char> &buffer);
+} // certbot
 #endif //CERT_UTILS_H
